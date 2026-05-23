7f26cfbe4961a481ebd26b649819ac71bfde5c1a
vitorf8 <vitor.rf08@gmail.com>
2026-05-23 17:52:17 -0300

    atualizacao final - py

--- Challenge-Python/challenge.py
+++ Challenge-Python/challenge.py
@@ -3,81 +3,102 @@ import time
 pontos = 0
 qtd_pontos = 0
 bilhetes = 0
-
+historico = []
+lista_menu = ("[1] Consultar pontos","[2] Adicionar pontos","[3] Atualizar pontos","[4] Excluir pontos",
+              "[5] Conversões em bilhetes","[6] Consultar histórico de tarefas","[0] Sair")
 
 def menu():
-    print("\n - - - - | MENU | - - - -")
-    print("[1] Consultar pontos")
-    print("[2] Adicionar pontos")
-    print("[3] Atualizar pontos")
-    print("[4] Excluir pontos")
-    print("[5] Conversões")
-    print("[0] Sair \n")
+    print("\n - - - - - - | MENU | - - - - - - -")
+    for item in lista_menu:
+        print (item)
+    return input("Escolha uma opção: ")
+
+def ler_inteiro(msg):
+    while True:
+        try:
+            return int(input(msg))
+        except ValueError:
+            print("Valor inválido! Digite apenas números inteiros.")
 
 
-print(" - - - - - - - - - - - - - - - ")
-print("|     U R B A N   L I N K     |")
-print(" - - - - - - - - - - - - - - - ")
+print(" - - - - - - - - - - - - - - - - - - - ")
+print("|         U R B A N   L I N K         |")
+print(" - - - - - - - - - - - - - - - - - - - ")
 
-
-menu()
-opcao = input("Escolha uma opção\n")
+opcao = menu()
 
 while opcao != "0":
 
-    if opcao == "1":
-        print(f"Pontos acumulados: \n{int(pontos)}")
+    match opcao:
 
-    elif opcao == "2":
-        qtd_pontos = float(input("Digite a quantidade de pontos que você deseja adicionar:\n"))
+        case "1":
+            print("------------------------------------")
+            print(f"Pontos acumulados: {pontos}")
+            print("------------------------------------")
+            historico.append("Consulta de pontos")
 
-        while qtd_pontos <= 0 or (qtd_pontos % 1) != 0:
-            qtd_pontos = float(input("Digite um valor inteiro e maior que zero:\n"))
+        case "2":
+            print("----------------------------------------------------")
+            qtd_pontos = ler_inteiro("Digite a quantidade de pontos que deseja adicionar:\n")
 
-        pontos += qtd_pontos
-        print("Pontos adicionados!")
-        qtd_pontos = 0
+            while qtd_pontos <= 0:
+                qtd_pontos = ler_inteiro("Digite um valor inteiro maior que zero:\n")
 
+            pontos += qtd_pontos
+            print("Pontos adicionados com sucesso!")
+            print("----------------------------------------------------")
+            historico.append("Adição de pontos")
 
-    elif opcao == "3":
-        qtd_pontos = float(input("Digite a nova quantidade do seu saldo de pontos:\n"))
+        case "3":
+            print("----------------------------------------------")
+            qtd_pontos = ler_inteiro("Digite a nova quantidade do saldo de pontos:\n")
 
-        while qtd_pontos <= 0 or (qtd_pontos % 1) != 0:
-            qtd_pontos = float(input("Digite um valor inteiro e maior(ou igual) a zero:\n"))
-            
-        pontos = qtd_pontos
-        print("Pontos atualizados!")
-        qtd_pontos = 0
+            while qtd_pontos < 0:
+                qtd_pontos = ler_inteiro("Digite um valor inteiro maior ou igual a zero:\n")
 
+            pontos = qtd_pontos
+            print("Pontos atualizados com sucesso!")
+            print("----------------------------------------------")
+            historico.append("Atualização de pontos")
 
-    elif opcao == "4":
-        
-        if pontos > 0:
+        case "4":
+            if pontos > 0:
+                print("---------------------------------------------------")
+                qtd_pontos = ler_inteiro("Digite a quantidade de pontos que deseja excluir:\n")
 
-            qtd_pontos = float(input("Digite a quantidade de pontos que você deseja excluir: \n"))
+                while qtd_pontos < 0 or qtd_pontos > pontos:
+                    qtd_pontos = ler_inteiro("Valor inválido! Não pode ser negativo nem maior que o saldo:\n")
 
-            while qtd_pontos > pontos or (qtd_pontos % 1) != 0:
-                qtd_pontos = float(input("O valor deve ser inteiro e não pode ser maior que o seu saldo de pontos agora!: \n Digite novamente \n"))
+                pontos -= qtd_pontos
+                print("Pontos excluídos com sucesso!")
+                print("---------------------------------------------------")
+                historico.append("Exclusão de pontos")
 
-            print("Pontos excluídos.")
-            qtd_pontos = 0
+            else:
+                print("Você não possui pontos para excluir.")
 
-        else:
-            print("Não é possível excluir pontos, pois não há saldo.")
+        case "5":
+            print("------------------------------------")
+            bilhetes = int(pontos * 0.05)
+            print(f"Pontos acumulados: {pontos}")
+            print(f"Conversão em bilhetes: {bilhetes}")
+            print("------------------------------------")
+            historico.append("Consulta de bilhetes")
 
+        case "6":
+            print("\n- - - HISTÓRICO DE TAREFAS(DA MAIS ANTIGA ATÉ A MAIS RECENTE): - - -\n")
+            cont = 1
 
-    elif opcao == "5":
-        bilhetes = int(pontos*0.05)
-        print(f"Quantidade de pontos acumuldados:{int(pontos)} \nConversão em bilhetes: \n{bilhetes}")
-        
-        
-    else:
-        print("Opção inválida, tente novamenete.\n")
+            for item in historico:
+                print(f"{cont}º {item}")
+                cont +=1
+            historico.append("Consulta do Histórico de Tarefas")
+            print("\n- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n")
 
+        case _:
+            print("Opção inválida! Tente novamente.")
 
-    menu()
-    opcao = input("Escolha uma opção\n")
+    opcao = menu()
 
-
-print("Saindo do sistema...")
+print("\nSaindo do sistema...")
 time.sleep(2)
